3a0a02c44f4aff9b0d6111ce072e35b42c1b48e5
Daydream0929 <2352869723@qq.com>
2024-01-17 17:48:29 +0800

    test(zen3): add zen3-sgemm vs openblas-sgemm

---
+++ test/zen3/sgemm_test.cc
@@ -0,0 +1,43 @@
+// #include <benchmark/benchmark.h>
+// #include <cblas.h>
+// #include "zen3/gemm.h"
+
+// static void BM_zne3_sgemm(benchmark::State& state) {
+//     int N = state.range(0);
+//     float *A = new float[N * N];
+//     float *B = new float[N * N];
+//     float *C = new float[N * N];
+
+//     // 初始化 A 和 B
+//     // ...
+
+//     for (auto _ : state) {
+//         gemm::zen3::sgemm('N', 'N', N, N, N, 1.0f, A, N, B, N, 0.0f, C, N);
+//     }
+
+//     delete[] A;
+//     delete[] B;
+//     delete[] C;
+// }
+// BENCHMARK(BM_zne3_sgemm)->Range(8, 8<<10);
+
+// static void BM_OpenBLAS_sgemm(benchmark::State& state) {
+//     int N = state.range(0);
+//     float *A = new float[N * N];
+//     float *B = new float[N * N];
+//     float *C = new float[N * N];
+
+//     // 初始化 A 和 B
+//     // ...
+
+//     for (auto _ : state) {
+//         cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1.0f, A, N, B, N, 0.0f, C, N);
+//     }
+
+//     delete[] A;
+//     delete[] B;
+//     delete[] C;
+// }
+// BENCHMARK(BM_OpenBLAS_sgemm)->Range(8, 8<<10);
+
+// BENCHMARK_MAIN();
