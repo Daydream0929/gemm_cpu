822f85851a1738409e5e352e200a1e90ed77d7e6
Daydream0929 <2352869723@qq.com>
2024-01-21 23:55:24 +0800

    optimize 1.0

--- zen3/src/gemm.cc
+++ zen3/src/gemm.cc
@@ -9,23 +9,15 @@ void gemm::zen3::AddDot1x4(int k, int alpha, int beta, const float* A, int lda, 
 {
     int p;
     
-    *C *= beta;
+    C(0, 0) *= beta;
+    C(0, 1) *= beta;
+    C(0, 2) *= beta;
+    C(0, 3) *= beta;
+
     for (p = 0; p < k; p ++) {
         C(0, 0) += alpha * A(0, p) * B(p, 0);
-    }
-
-    *C *= beta;
-    for (p = 0; p < k; p ++) {
         C(0, 1) += alpha * A(0, p) * B(p, 1);
-    }
-
-    *C *= beta;
-    for (p = 0; p < k; p ++) {
         C(0, 2) += alpha * A(0, p) * B(p, 2);
-    }
-
-    *C *= beta;
-    for (p = 0; p < k; p ++) {
         C(0, 3) += alpha * A(0, p) * B(p, 3);
     }
 
@@ -50,17 +42,7 @@ void gemm::zen3::sgemm(
     int i, j, p;
     for (j = 0; j < n; j += 4) {
         for (i = 0; i < m; i ++) {
-            // AddDot1x4(k, alpha, beta, &A(i, 0), lda, &B(0, j), ldb, &C(i, j), ldc);
-            C(i, j)     += beta * C(i, j);
-            C(i, j + 1) += beta * C(i, j + 1);
-            C(i, j + 2) += beta * C(i, j + 2);
-            C(i, j + 3) += beta * C(i, j + 3);
-            for (p = 0; p < k; p ++) {
-                C(i, j) += alpha * A(i, p) * B(p, j);
-                C(i, j + 1) += alpha * A(i, p) * B(p, j + 1);
-                C(i, j + 2) += alpha * A(i, p) * B(p, j + 2);
-                C(i, j + 3) += alpha * A(i, p) * B(p, j + 3);
-            }
+            AddDot1x4(k, alpha, beta, &A(i, 0), lda, &B(0, j), ldb, &C(i, j), ldc);
         }
     }
 }
