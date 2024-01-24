e7ca10ad02f10f245ca0e7b36e22f249c0e75154
Daydream0929 <2352869723@qq.com>
2024-01-24 14:36:32 +0800

    feat(gemm): use register AddDot4x4

--- zen3/src/gemm.cc
+++ zen3/src/gemm.cc
@@ -68,27 +68,52 @@ void gemm::zen3::AddDot4x4(int k, int alpha, int beta, const float *A, int lda,
                            const float *B, int ldb, float *C, int ldc)
 {
     int p;
+
+    register float 
+        c_00_reg, c_01_reg, c_02_reg, c_03_reg,
+        c_10_reg, c_11_reg, c_12_reg, c_13_reg,
+        c_20_reg, c_21_reg, c_22_reg, c_23_reg,
+        c_30_reg, c_31_reg, c_32_reg, c_33_reg;
+
+    register float 
+        a_0p_reg, a_1p_reg, a_2p_reg, a_3p_reg;
+
+    c_00_reg = 0.0f, c_01_reg = 0.0f, c_02_reg = 0.0f, c_03_reg = 0.0f;
+    c_10_reg = 0.0f, c_11_reg = 0.0f, c_12_reg = 0.0f, c_13_reg = 0.0f;
+    c_20_reg = 0.0f, c_21_reg = 0.0f, c_22_reg = 0.0f, c_23_reg = 0.0f;
+    c_30_reg = 0.0f, c_31_reg = 0.0f, c_32_reg = 0.0f, c_33_reg = 0.0f;
+
     for (p = 0; p < k; p ++ ) {
-        C(0, 0) += A(0, p) * B(p, 0);
-        C(0, 1) += A(0, p) * B(p, 1);     
-        C(0, 2) += A(0, p) * B(p, 2);     
-        C(0, 3) += A(0, p) * B(p, 3); 
+        a_0p_reg = A(0, p);
+        a_1p_reg = A(1, p);
+        a_2p_reg = A(2, p);
+        a_3p_reg = A(3, p);
 
-        C(1, 0) += A(1, p) * B(p, 0);
-        C(1, 1) += A(1, p) * B(p, 1);     
-        C(1, 2) += A(1, p) * B(p, 2);     
-        C(1, 3) += A(1, p) * B(p, 3); 
+        c_00_reg += a_0p_reg * B(p, 0);
+        c_01_reg += a_0p_reg * B(p, 1);     
+        c_02_reg += a_0p_reg * B(p, 2);     
+        c_03_reg += a_0p_reg * B(p, 3); 
 
-        C(2, 0) += A(2, p) * B(p, 0);
-        C(2, 1) += A(2, p) * B(p, 1);     
-        C(2, 2) += A(2, p) * B(p, 2);     
-        C(2, 3) += A(2, p) * B(p, 3); 
+        c_10_reg += a_1p_reg * B(p, 0);
+        c_11_reg += a_1p_reg * B(p, 1);     
+        c_12_reg += a_1p_reg * B(p, 2);     
+        c_13_reg += a_1p_reg * B(p, 3); 
 
-        C(3, 0) += A(3, p) * B(p, 0);
-        C(3, 1) += A(3, p) * B(p, 1);     
-        C(3, 2) += A(3, p) * B(p, 2);     
-        C(3, 3) += A(3, p) * B(p, 3); 
+        c_20_reg += a_2p_reg * B(p, 0);
+        c_21_reg += a_2p_reg * B(p, 1);     
+        c_22_reg += a_2p_reg * B(p, 2);     
+        c_23_reg += a_2p_reg * B(p, 3); 
+
+        c_30_reg += a_3p_reg * B(p, 0);
+        c_31_reg += a_3p_reg * B(p, 1);     
+        c_32_reg += a_3p_reg * B(p, 2);     
+        c_33_reg += a_3p_reg * B(p, 3); 
     }
+
+    C(0, 0) += c_00_reg, C(0, 1) += c_01_reg, C(0, 2) += c_02_reg, C(0, 3) += c_03_reg;
+    C(1, 0) += c_10_reg, C(1, 1) += c_11_reg, C(1, 2) += c_12_reg, C(1, 3) += c_13_reg;
+    C(2, 0) += c_20_reg, C(2, 1) += c_21_reg, C(2, 2) += c_22_reg, C(2, 3) += c_23_reg;
+    C(3, 0) += c_30_reg, C(3, 1) += c_31_reg, C(3, 2) += c_32_reg, C(3, 3) += c_33_reg;
 }
 
 void gemm::zen3::sgemm(const char transa, const char transb, const int m,
