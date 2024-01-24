dc3f65840eda30ccb1f26110f028f41ab85ea1d5
Daydream0929 <2352869723@qq.com>
2024-01-24 15:41:01 +0800

    feat(gemm): optimize 5 :use register for b_pointer

--- zen3/src/gemm.cc
+++ zen3/src/gemm.cc
@@ -76,7 +76,8 @@ void gemm::zen3::AddDot4x4(int k, int alpha, int beta, const float *A, int lda,
         c_30_reg, c_31_reg, c_32_reg, c_33_reg;
 
     register float 
-        a_0p_reg, a_1p_reg, a_2p_reg, a_3p_reg;
+        a_0p_reg, a_1p_reg, a_2p_reg, a_3p_reg,
+        b_p0_reg, b_p1_reg, b_p2_reg, b_p3_reg;
     
     const float 
         *bp0_pntr, *bp1_pntr, *bp2_pntr, *bp3_pntr;
@@ -86,36 +87,41 @@ void gemm::zen3::AddDot4x4(int k, int alpha, int beta, const float *A, int lda,
     c_20_reg = 0.0f, c_21_reg = 0.0f, c_22_reg = 0.0f, c_23_reg = 0.0f;
     c_30_reg = 0.0f, c_31_reg = 0.0f, c_32_reg = 0.0f, c_33_reg = 0.0f;
 
+    bp0_pntr = &B(0, 0);
+    bp1_pntr = &B(0, 1);
+    bp2_pntr = &B(0, 2);
+    bp3_pntr = &B(0, 3);
+
     for (p = 0; p < k; p ++ ) {
         a_0p_reg = A(0, p);
         a_1p_reg = A(1, p);
         a_2p_reg = A(2, p);
         a_3p_reg = A(3, p);
 
-        bp0_pntr = &B(p, 0);
-        bp1_pntr = &B(p, 1);
-        bp2_pntr = &B(p, 2);
-        bp3_pntr = &B(p, 3);
+        b_p0_reg = *bp0_pntr++;
+        b_p1_reg = *bp1_pntr++;
+        b_p2_reg = *bp2_pntr++;
+        b_p3_reg = *bp3_pntr++;
 
-        c_00_reg += a_0p_reg * *bp0_pntr;
-        c_01_reg += a_0p_reg * *bp1_pntr;    
-        c_02_reg += a_0p_reg * *bp2_pntr;    
-        c_03_reg += a_0p_reg * *bp3_pntr; 
+        c_00_reg += a_0p_reg * b_p0_reg;
+        c_01_reg += a_0p_reg * b_p1_reg;    
+        c_02_reg += a_0p_reg * b_p2_reg;    
+        c_03_reg += a_0p_reg * b_p3_reg; 
 
-        c_10_reg += a_1p_reg * *bp0_pntr;
-        c_11_reg += a_1p_reg * *bp1_pntr;    
-        c_12_reg += a_1p_reg * *bp2_pntr;     
-        c_13_reg += a_1p_reg * *bp3_pntr; 
+        c_10_reg += a_1p_reg * b_p0_reg;
+        c_11_reg += a_1p_reg * b_p1_reg;    
+        c_12_reg += a_1p_reg * b_p2_reg;     
+        c_13_reg += a_1p_reg * b_p3_reg; 
 
-        c_20_reg += a_2p_reg * *bp0_pntr;
-        c_21_reg += a_2p_reg * *bp1_pntr;    
-        c_22_reg += a_2p_reg * *bp2_pntr;     
-        c_23_reg += a_2p_reg * *bp3_pntr; 
+        c_20_reg += a_2p_reg * b_p0_reg;
+        c_21_reg += a_2p_reg * b_p1_reg;    
+        c_22_reg += a_2p_reg * b_p2_reg;     
+        c_23_reg += a_2p_reg * b_p3_reg; 
 
-        c_30_reg += a_3p_reg * *bp0_pntr;
-        c_31_reg += a_3p_reg * *bp1_pntr;    
-        c_32_reg += a_3p_reg * *bp2_pntr;     
-        c_33_reg += a_3p_reg * *bp3_pntr;
+        c_30_reg += a_3p_reg * b_p0_reg;
+        c_31_reg += a_3p_reg * b_p1_reg;    
+        c_32_reg += a_3p_reg * b_p2_reg;     
+        c_33_reg += a_3p_reg * b_p3_reg;
     }
 
     C(0, 0) += c_00_reg, C(0, 1) += c_01_reg, C(0, 2) += c_02_reg, C(0, 3) += c_03_reg;
